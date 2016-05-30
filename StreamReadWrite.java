3b3e5723b540da41f4a9c6b0fabcd1894f6f0035
vs77 <vvitaliys@hotmail.com>
2016-05-30 10:31:47 -0400

    Add files via upload

--- StreamReadWrite.java
+++ StreamReadWrite.java
@@ -1,9 +1,14 @@
 package fileOps;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
 import java.nio.file.Path;
@@ -15,10 +20,13 @@ public class StreamReadWrite {
     public StreamReadWrite() {
        
     }
-    
+    private static final String FILE_NAME = "c://files//BritishBands.txt";
+            
     public static void main (String[] argv){
+        
+        
         try{
-            createNewFile(20,"c://files//BritishBands.txt");
+            createNewFile(20,FILE_NAME);
         }
         catch(IOException|NullPointerException ex){
             ex.printStackTrace();
@@ -45,6 +53,24 @@ public class StreamReadWrite {
             ex.printStackTrace();
             throw ex;
         }
+        
+        System.out.println("There are " + lineCount(FILE_NAME) + " lines in the file : " + FILE_NAME);
                 
     }
-}
+    
+    private static long lineCount(String fileName) throws IOException{
+        long lineCount = 0;
+        File f = new File(fileName);
+        FileInputStream fis = new FileInputStream(f);
+        BufferedReader  br  = new BufferedReader(new InputStreamReader(fis));
+        
+        return br.lines().count();
+        
+    }
+    
+    private static void createNewFileStream(long lineNum, String fileName) throws IOException{
+        
+    }
+    
+        
+        }
